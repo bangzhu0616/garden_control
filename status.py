ee2acd15e62b76690d3c69e2de1bb0ab781406fb
Xiaofeng Hao <xfhaome@gmail.com>
2019-10-09 12:10:03 -0700

    fix return value of status

--- status.py
+++ status.py
@@ -1,3 +1,4 @@
 def set_status(l_on, h_on, l_should_on, t_l, t_h):
     light_status = l_should_on
     heater_status = t_l or (h_on and (not t_l) and (not t_h))
+    return light_status, heater_status
